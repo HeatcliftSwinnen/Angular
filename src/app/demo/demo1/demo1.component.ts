b99a399b438edcddc7ed694da8a191ca43e118a1
Aude Beurive <aude.beurive@bstorm.be>
2023-06-16 15:34:24 +0200

    demo bindings

--- src/app/demo/demo1/demo1.component.ts
+++ src/app/demo/demo1/demo1.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo1.component.scss']
 })
 export class Demo1Component {
+    firstname : string = "Aude";
 
+    lastname : string = "";
+
+    message : string = "[... en attente d'un bonjour ...]";
+    
+    isActive : boolean = false;
+
+    monPlaceholder : string = "Pouet";
+
+    monUrlImg : string = "./assets/images/coubeh.jpg";
+
+    direBonjour(langue : string) : void {
+      switch(langue) {
+        case 'fr' :
+          this.message = "Bonjour !";
+          break;
+        case 'nl' :
+          this.message = "Goedendag !";
+          break;
+        case 'de' :
+          this.message = "Guten Tag !";
+          break;
+        default :
+          this.message = "Quoi ?";
+          break;
+      }
+    }
+
+    toggleIsActive() : void {
+      this.isActive = !this.isActive;
+    }
 }
