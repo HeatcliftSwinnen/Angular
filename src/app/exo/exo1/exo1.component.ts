6627b08f829df901c4e42dfab660313738c9b3d3
Aude Beurive <aude.beurive@bstorm.be>
2023-06-19 11:56:20 +0200

    mise en place exo chrono

--- src/app/exo/exo1/exo1.component.ts
+++ src/app/exo/exo1/exo1.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class Exo1Component {
 
+  seconds : number = 0;
+  timer : any = undefined;
+
+  start() : void {
+    this.timer = setInterval(() => {
+      this.seconds++;
+    }, 1000)
+  }
+
+  pause() : void {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  }
+
+  reset() : void {
+    this.seconds = 0;
+    this.pause();
+  }
 }
