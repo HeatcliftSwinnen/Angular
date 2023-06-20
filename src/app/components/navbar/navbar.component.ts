2a1eb3d1f5ee7265cda365ecce257d255e12d9e5
Aude Beurive <aude.beurive@bstorm.be>
2023-06-20 09:30:06 +0200

    exo navbar directives

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from 'src/app/shared/models/link';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  navLinks : Link[] = [
+    { title : 'Accueil', url : '/', isVisible : false },
+    { title : 'Demos', url : '/demo', children : [
+      { title : 'Demo 1 - Les Bindings', url : '/demo/demo1'},
+      { title : 'Demo 2 - Les Pipes', url : '/demo/demo2'},
+      { title : 'Demo 3 - Les Directives', url : '/demo/demo3'}
+    ], isVisible : false },
+    { title : 'Exos', url : '/exo', children : [
+      { title : 'Exo 1 - Le Chrono', url : '/exo/exo1'}
+    ], isVisible : false },
+    { title : 'Rappels', url : '/rappels', isVisible : false }
+  ]
   
+  
+  toggleVisible(link : Link) : void {
+    // On stock l'état de isVisible du lien sur lequel on vient de cliquer
+    let currentState = link.isVisible;
+
+    // On parcourt chacun des liens "parents" du tableau navLinks et change leur propriété isVisible en false (ferme tout)
+    this.navLinks.forEach(l => l.isVisible = false)
+
+    // On met la prop isVisible du lien sur lequel on vient de cliquer à son état contraire
+    link.isVisible = !currentState;
+  }
+
 }
