fe2a84d75ed5c689c0f1598b08335ab82691e0d9
Aude Beurive <aude.beurive@bstorm.be>
2023-06-23 11:26:35 +0200

    formulaire demo fin

--- src/app/demo/demo6/demo6.component.ts
+++ src/app/demo/demo6/demo6.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-demo6',
@@ -8,10 +8,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class Demo6Component {
   contactForm : FormGroup;
+  countries : string[] = ["belgique","allemagne","france"];
+
 
   constructor(private _fb : FormBuilder) {
     this.contactForm = this._fb.group({
       //nomControl : [valeur, [Validateurs synchrones] , [Validateurs asynchrones]]
+      //password : [null, [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]]
       lastname : [null, [ Validators.required, Validators.minLength(2), Validators.maxLength(100) ]],
       firstname : [null, [ Validators.required, Validators.minLength(2), Validators.maxLength(100) ]],
       birthdate : [null, [ Validators.required ]],
@@ -20,8 +23,25 @@ export class Demo6Component {
       couple : [false],
       gender : ['male', [Validators.required]],
       type : ['', [Validators.required]],
-      //password : [null, [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]]
+      country : [null, [Validators.required, this.countryValidator()]],
+      hobbies : this._fb.array([
+        this._fb.control(null, Validators.required)
+      ])
     })
+  }
+
+  //Renverra le control qui s'appelle hobbies, qui est un tableau de controls
+  //On s'en servira pour boucler dessus
+  get hobbies() : FormArray {
+    return this.contactForm.get('hobbies') as FormArray;
+  }
+
+  addHobby() {
+    this.hobbies.push(this._fb.control(null, Validators.required))
+  }
+
+  removeHobby(i : number) {
+    this.hobbies.removeAt(i);
   }
 
   addContact() {
@@ -33,5 +53,15 @@ export class Demo6Component {
       //Du coup toutes nos erreurs requis vont apparaitre
       this.contactForm.markAllAsTouched();
     }    
+  }
+
+  countryValidator() : ValidatorFn | null {
+    return (control : AbstractControl) => {
+      if(control.value && !this.countries.includes(control.value?.trim().toLowerCase())){
+        //erreur
+        return { invalidcountry : true }
+      }
+      return null;
+    }
   }
 }
