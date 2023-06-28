20ebabb8fd8955e75cd4cb1682fad8dec58eb326
Aude Beurive <aude.beurive@bstorm.be>
2023-06-28 14:52:54 +0200

    correction genre api nationalize api

--- src/app/shared/guards/connected.guard.ts
+++ src/app/shared/guards/connected.guard.ts
@@ -1,9 +1,23 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { FakeAuthService } from '../services/fake-auth.service';
+import { map } from 'rxjs';
+
 
 export const connectedGuard: CanActivateFn = (route, state) => {
-  //Normalement ici, on fait genre des vérifications de si on a un user connecté, on reviendra faire ça quand on aura vu les observables et le storage
+  const router = inject(Router)
+  const fakeAuthService = inject(FakeAuthService)
 
+  //Normalement ici, on fait genre des vérifications de si on a un user connecté, on reviendra faire ça quand on aura vu les observables et le storage
   //La guard, renvoie juste un boolean vrai ou faux, pour savoir si peut accéder au composant
-  return false;
+  return fakeAuthService.$connectedUser.pipe(map((res) => { 
+    if(res) {
+      return true;
+    }
+    else {
+      router.navigateByUrl('/demo/demo5');
+      return false;
+    }
+  }));
   //Souvent, plutôt que renvoyer faux, on va faire une redirection (vers une page login...une page Teuh teuh teuh vous n'êtes pas admin etc)
 };
